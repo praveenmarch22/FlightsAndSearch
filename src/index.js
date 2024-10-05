7284ef2a971b281e3b1a3f07fe0b65b6703621be
praveenmarch22 <praveen.220302@gmail.com>
2024-10-05 18:32:57 +0530

    Set up a basic db congif

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const { PORT } = require("./config/serverConfig");
 
 const StartTheServer = async () => {
   const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
